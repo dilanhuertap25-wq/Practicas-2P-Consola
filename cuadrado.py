f1ff732744b002e2b05f1094df92f13e8ae992a0
Dylan <dilanhuertap25@cetis146.edu.mx>
2026-03-20 08:05:22 -0600

    Eliminar cuadrado.py

--- cuadrado.py
+++
@@ -1,9 +0,0 @@
-class Cuadrado:
-    def __init__(self, lado):
-        self.lado = lado
-
-    def calcular_area(self):
-        return self.lado ** 2
-
-    def calcular_perimetro(self):
-        return 4 * self.lado
